963bc76fac69a48f7bc39549df049d268550915a
N <n@push0.dev>
2023-09-26 13:39:10 -0400

    inital

--- contracts/src/invoke/Issuance.sol
+++ contracts/src/invoke/Issuance.sol
@@ -94,6 +94,10 @@ contract Issuance {
         vault.invokeERC20s(args);
     }
 
+    /// @notice Quote the amount of underlying tokens needed to issue index tokens
+    /// @param amount The amount of index tokens to issue
+    /// @return tokens The underlying tokens and amounts needed to issue index tokens
+    /// @dev subtract 1 from each unit to account for rounding if you need an exact redemption quote
     function quote(uint256 amount) external view returns (TokenInfo[] memory) {
         TokenInfo[] memory tokens = vault.realUnits();
 
