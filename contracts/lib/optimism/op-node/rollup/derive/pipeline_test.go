0e718f47f767e605ba6b53bfd0d179adc9a96f30
junhohong <jun@junho.co>
2023-09-28 14:20:25 -0700

    remove unused libs

--- contracts/lib/optimism/op-node/rollup/derive/pipeline_test.go
+++
@@ -1,9 +0,0 @@
-package derive
-
-import "github.com/ethereum-optimism/optimism/op-node/testutils"
-
-var _ Engine = (*testutils.MockEngine)(nil)
-
-var _ L1Fetcher = (*testutils.MockL1Source)(nil)
-
-var _ Metrics = (*testutils.TestDerivationMetrics)(nil)
