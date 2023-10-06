e4055c9829846211e9e44501904764ffa2abcc8d
junhohong <jun@junho.co>
2023-10-06 13:13:02 -0700

    use interfaces for ActiveBounty, InvokeableBounty, and IRebalancer

---
+++ contracts/src/interfaces/IInvokeableBounty.sol
@@ -0,0 +1,32 @@
+pragma solidity =0.8.18;
+import {TokenInfo} from "src/Common.sol";
+
+struct Bounty {
+    TokenInfo[] infos;
+    address fulfiller;
+    uint256 deadline;
+    bytes32 salt;
+}
+
+struct QuoteInput {
+    TokenInfo[] targets;
+    uint256 supply;
+}
+
+interface IInvokeableBounty {
+    error BountyInvalidHash();
+    error BountyAlreadyCompleted();
+    error BountyPastDeadline();
+    error BountyAMKTSupplyChange();
+    error BountyReentrant();
+    error BountyMustIncludeAllUnderlyings();
+    error BountyInvalidFulfiller();
+
+    event BountyFulfilled(Bounty bounty, bool callback);
+
+    function fulfillBounty(Bounty memory bounty, bool callback) external;
+
+    function hashBounty(
+        Bounty memory bounty
+    ) external view returns (bytes32 hash);
+}
---
+++ contracts/src/interfaces/IRebalancer.sol
@@ -0,0 +1,9 @@
+pragma solidity =0.8.18;
+import {TokenInfo} from "src/Common.sol";
+
+interface IRebalancer {
+    function rebalanceCallback(
+        TokenInfo[] calldata required,
+        TokenInfo[] calldata received
+    ) external;
+}
