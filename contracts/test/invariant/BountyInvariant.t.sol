6c14a38b16daa5eed410dca6b71303358cb689de
junhohong <jun@junho.co>
2023-10-17 17:15:01 -0700

    add bounty invariant test

--- contracts/test/invariant/BountyInvariant.t.sol
+++ contracts/test/invariant/BountyInvariant.t.sol
@@ -1,7 +1,12 @@
 pragma solidity =0.8.18;
 
-import {UpgradedTest} from "test/upgrade/helpers/Upgraded.t.sol";
-import {Dealer} from "test/utils/Dealer.t.sol";
+import {StatefulTest} from "core-test/State.t.sol";
+import {TokenInfo} from "src/Common.sol";
+import {IERC20} from "forge-std/interfaces/IERC20.sol";
+import {SCALAR, fmul} from "src/lib/FixedPoint.sol";
+import {IVault} from "src/interfaces/IVault.sol";
+import {IIssuance} from "src/interfaces/IIssuance.sol";
+import {MockMintableToken} from "test/utils/MockMintableToken.sol";
 import {TokenInfo} from "src/Common.sol";
 import {IERC20} from "forge-std/interfaces/IERC20.sol";
 import {InvokeableBounty} from "src/invoke/Bounty.sol";
@@ -12,7 +17,94 @@ import {fmul} from "src/lib/FixedPoint.sol";
 import {console} from "forge-std/console.sol";
 import {IActiveBounty} from "src/interfaces/IActiveBounty.sol";
 
-contract UpgradedBountyTest is UpgradedTest {
+contract BountyInvariantTest is StatefulTest {
+    InvokeableBounty invokeableBounty;
+    address[10] mockTokens;
+
+    function setUp() public override {
+        super.setUp();
+        invokeableBounty = InvokeableBounty(bounty);
+        seedInitial(10);
+        excludeContract(address(indexToken));
+        // add new tokens
+        for (uint256 i = 0; i < 10; i++) {
+            MockMintableToken newToken = new MockMintableToken(
+                "TEST",
+                "TEST",
+                18,
+                0
+            );
+            mockTokens[i] = address(newToken);
+        }
+    }
+
+    function _satisfyFulfillerBalances(
+        address fulfiller,
+        Bounty memory bounty
+    ) internal {
+        (, TokenInfo[] memory ins) = quoter.quoteFulfillBounty(
+            bounty,
+            indexToken.totalSupply()
+        );
+        for (uint256 i = 0; i < ins.length; i++) {
+            if (ins[i].units == 0) {
+                continue;
+            }
+            IERC20(ins[i].token).approve(
+                address(invokeableBounty),
+                type(uint256).max
+            );
+            MockMintableToken(ins[i].token).mint(fulfiller, ins[i].units);
+        }
+    }
+
+    function _generateValidBounty(
+        uint256 numTokensToAdd,
+        uint256 rand
+    ) internal view returns (Bounty memory) {
+        TokenInfo[] memory currentUnits = vault.virtualUnits();
+        TokenInfo[] memory newUnits = new TokenInfo[](
+            currentUnits.length + numTokensToAdd
+        );
+        // randomize old tokens
+        for (uint256 i = 0; i < currentUnits.length; i++) {
+            if (rand % 10 == 0) {
+                newUnits[i] = TokenInfo({
+                    token: currentUnits[i].token,
+                    units: 0
+                });
+            } else if (rand % 2 == 0) {
+                newUnits[i] = TokenInfo({
+                    token: currentUnits[i].token,
+                    units: currentUnits[i].units +
+                        (rand % currentUnits[i].units)
+                });
+            } else {
+                // this can also result in 0 units
+                newUnits[i] = TokenInfo({
+                    token: currentUnits[i].token,
+                    units: currentUnits[i].units -
+                        (rand % currentUnits[i].units)
+                });
+            }
+        }
+        // add new tokens
+        for (uint256 i = currentUnits.length; i < newUnits.length; i++) {
+            newUnits[i] = TokenInfo({
+                token: mockTokens[newUnits.length - currentUnits.length - 1],
+                units: (rand % 1e18)
+            });
+        }
+
+        Bounty memory bounty = Bounty({
+            infos: newUnits,
+            fulfiller: address(this),
+            salt: keccak256("test"),
+            deadline: block.timestamp + 1000
+        });
+        return bounty;
+    }
+
     function _setUpTestBountyInvariantFuzz(
         uint256 numTokensToAdd,
         uint256 rand
@@ -33,17 +125,17 @@ contract UpgradedBountyTest is UpgradedTest {
         // generate a valid bounty
         Bounty memory bounty = _generateValidBounty(numTokensToAdd, rand);
         TokenInfo[] memory proposedUnits = bounty.infos;
-        bytes32 _hash = timelockInvokeableBounty.hashBounty(bounty);
+        bytes32 _hash = invokeableBounty.hashBounty(bounty);
 
-        // set bounty by anyone except timelock should fail
+        // set bounty by anyone except fulfiller should fail
         vm.startPrank(address(3));
         vm.expectRevert(IActiveBounty.ActiveBountyAuth.selector);
-        timelockActiveBounty.setHash(_hash);
+        activeBounty.setHash(_hash);
         vm.stopPrank();
 
-        // set bounty by timelock should succeed
-        vm.prank(address(timelockController));
-        timelockActiveBounty.setHash(_hash);
+        // set bounty by authority should succeed
+        vm.prank(authority);
+        activeBounty.setHash(_hash);
         _satisfyFulfillerBalances(address(this), bounty);
 
         // store balances for old and new tokens for vault and fulfiller (this contract) before fulfilling
@@ -75,11 +167,11 @@ contract UpgradedBountyTest is UpgradedTest {
         // fulfill bounty by anyone except fulfiller should fail
         vm.startPrank(address(4));
         vm.expectRevert(IInvokeableBounty.BountyInvalidFulfiller.selector);
-        timelockInvokeableBounty.fulfillBounty(bounty, false);
+        invokeableBounty.fulfillBounty(bounty, false);
         vm.stopPrank();
 
         // fulfill bounty by fulfiller should succeed
-        timelockInvokeableBounty.fulfillBounty(bounty, false);
+        invokeableBounty.fulfillBounty(bounty, false);
         return (
             oldUnits,
             proposedUnits,
@@ -90,10 +182,9 @@ contract UpgradedBountyTest is UpgradedTest {
         );
     }
 
-    function testBountyInvariantsFuzz(
-        uint256 numTokensToAdd,
-        uint256 rand
-    ) public {
+    function invariant_bounty_formula_reconstitution() public {
+        uint256 numTokensToAdd = 10;
+        uint256 rand = 1e25;
         (
             TokenInfo[] memory oldUnits,
             TokenInfo[] memory proposedUnits,
@@ -119,7 +210,7 @@ contract UpgradedBountyTest is UpgradedTest {
 
         // INVARIANT 2a: vault balance increases by fmul(targetUnits - virtualUnits + 1, totalSupply) + 1, fulfiller balance decreases by the same amount
         // INVARIANT 2b: fulfiller balance increases by fmul(virtualUnits - targetUnits, totalSupply), vault balance decreases by the same amount
-        uint256 totalSupply = AMKT.totalSupply();
+        uint256 totalSupply = indexToken.totalSupply();
         // for all old units, check if proposed units was greater than old units. if so, check that vault balance increased by the correct amount
         for (uint256 i = 0; i < oldUnits.length; i++) {
             if (oldUnits[i].units > proposedUnits[i].units) {
@@ -192,74 +283,5 @@ contract UpgradedBountyTest is UpgradedTest {
                 );
             }
         }
-    }
-
-    function _satisfyFulfillerBalances(
-        address fulfiller,
-        Bounty memory bounty
-    ) internal {
-        Dealer dealer = new Dealer();
-        (, TokenInfo[] memory ins) = quoter.quoteFulfillBounty(
-            bounty,
-            AMKT.totalSupply()
-        );
-        for (uint256 i = 0; i < ins.length; i++) {
-            if (ins[i].units == 0) {
-                continue;
-            }
-            IERC20(ins[i].token).approve(
-                address(timelockInvokeableBounty),
-                type(uint256).max
-            );
-            dealer.dealToken(ins[i].token, fulfiller, ins[i].units); // only deal exactly the units needed
-        }
-    }
-
-    function _generateValidBounty(
-        uint256 numTokensToAdd,
-        uint256 rand
-    ) internal returns (Bounty memory) {
-        TokenInfo[] memory currentUnits = vault.virtualUnits();
-        TokenInfo[] memory newUnits = new TokenInfo[](
-            currentUnits.length + numTokensToAdd
-        );
-        // randomize old tokens
-        for (uint256 i = 0; i < currentUnits.length; i++) {
-            if (rand % 10 == 0) {
-                newUnits[i] = TokenInfo({
-                    token: currentUnits[i].token,
-                    units: 0
-                });
-            } else if (rand % 2 == 0) {
-                newUnits[i] = TokenInfo({
-                    token: currentUnits[i].token,
-                    units: currentUnits[i].units +
-                        (rand % currentUnits[i].units)
-                });
-            } else {
-                // this can also result in 0 units
-                newUnits[i] = TokenInfo({
-                    token: currentUnits[i].token,
-                    units: currentUnits[i].units -
-                        (rand % currentUnits[i].units)
-                });
-            }
-        }
-        // add new tokens
-        for (uint256 i = currentUnits.length; i < newUnits.length; i++) {
-            newUnits[i] = TokenInfo({
-                token: address(new MockMintableToken("test", "test", 18, 0)),
-                units: (rand % 1e18)
-            });
-            console.log("adding: %s", newUnits[i].token);
-        }
-
-        Bounty memory bounty = Bounty({
-            infos: newUnits,
-            fulfiller: address(this),
-            salt: keccak256("test"),
-            deadline: block.timestamp + 1000
-        });
-        return bounty;
     }
 }
